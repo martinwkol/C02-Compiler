23072a0b694cb54fd496912aafbdaf4bff97f9aa
Martin Koloseus <mk_alternativ@posteo.de>
2025-05-08 16:48:39 +0200

    BinaryOperationInstructon & AddIntstruction only have 2 inputs

--- src/main/java/edu/kit/kastel/vads/compiler/backend/instruction/AddInstruction.java
+++ src/main/java/edu/kit/kastel/vads/compiler/backend/instruction/AddInstruction.java
@@ -3,9 +3,7 @@ package edu.kit.kastel.vads.compiler.backend.instruction;
 import edu.kit.kastel.vads.compiler.backend.register.Register;
 
 public class AddInstruction extends BinaryOperationInstruction {
-
-    public AddInstruction(Register destination, Register left, Register right) {
-        super(destination, left, right);
+    public AddInstruction(Register left, Register right) {
+        super(left, right);
     }
-
 }
--- src/main/java/edu/kit/kastel/vads/compiler/backend/instruction/BinaryOperationInstruction.java
+++ src/main/java/edu/kit/kastel/vads/compiler/backend/instruction/BinaryOperationInstruction.java
@@ -5,14 +5,12 @@ import edu.kit.kastel.vads.compiler.backend.register.Register;
 import java.util.List;
 
 public abstract class BinaryOperationInstruction extends Instruction {
-    protected Register destination;
     protected Register left;
     protected Register right;
 
-    protected BinaryOperationInstruction(Register destination, Register left, Register right) {
-        super(List.of(destination), List.of(left, right));
+    protected BinaryOperationInstruction(Register left, Register right) {
+        super(List.of(left), List.of(left, right));
 
-        this.destination = destination;
         this.left = left;
         this.right = right;
     }
