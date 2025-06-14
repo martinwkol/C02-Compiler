f97fb1b5f0ffd974aa8b4de21c0b6b09c45c89e1
Martin Koloseus <mkoloseus@posteo.de>
2025-06-14 12:31:02 +0200

    feat(IfNode): trueBlock, falseBlock

--- src/main/java/edu/kit/kastel/vads/compiler/ir/node/IfNode.java
+++ src/main/java/edu/kit/kastel/vads/compiler/ir/node/IfNode.java
@@ -1,14 +1,20 @@
 package edu.kit.kastel.vads.compiler.ir.node;
 
 public final class IfNode extends Node {
-    private final Block targetBlock;
+    private final Block trueBlock;
+    private final Block falseBlock;
 
-    public IfNode(Block block, Node condition, Block targetBlock) {
+    public IfNode(Block block, Node condition, Block trueBlock, Block falseBlock) {
         super(block, condition);
-        this.targetBlock = targetBlock;
+        this.trueBlock = trueBlock;
+        this.falseBlock = falseBlock;
     }
 
-    public Block targetBlock() {
-        return targetBlock;
+    public Block trueBlock() {
+        return trueBlock;
+    }
+
+    public Block falseBlock() {
+        return falseBlock;
     }
 }
