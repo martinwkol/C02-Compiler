ce1c1110639d904bc261d548b51d077380c0d0ee
Martin Koloseus <mkoloseus@posteo.de>
2025-06-16 18:13:46 +0200

    "fix"(ReturnNode): don't use side effect

--- src/main/java/edu/kit/kastel/vads/compiler/ir/node/ReturnNode.java
+++ src/main/java/edu/kit/kastel/vads/compiler/ir/node/ReturnNode.java
@@ -1,10 +1,11 @@
 package edu.kit.kastel.vads.compiler.ir.node;
 
 public final class ReturnNode extends ExitNode {
-    public static final int SIDE_EFFECT = 0;
-    public static final int RESULT = 1;
+    //public static final int SIDE_EFFECT = 0;
+    public static final int RESULT = 0;
     public ReturnNode(Block block, Node sideEffect, Node result) {
-        super(block, sideEffect, result);
+        // TODO: SideEffect
+        super(block, result);
     }
 
     @Override
