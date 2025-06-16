d120f1102c0406ddaf3d0703cb1e2a90528784ff
Martin Koloseus <mkoloseus@posteo.de>
2025-06-16 19:19:48 +0200

    feat(ReturnNode): reinstate side effect

--- src/main/java/edu/kit/kastel/vads/compiler/ir/node/ReturnNode.java
+++ src/main/java/edu/kit/kastel/vads/compiler/ir/node/ReturnNode.java
@@ -1,10 +1,9 @@
 package edu.kit.kastel.vads.compiler.ir.node;
 
 public final class ReturnNode extends ExitNode {
-    //public static final int SIDE_EFFECT = 0;
-    public static final int RESULT = 0;
+    public static final int SIDE_EFFECT = 0;
+    public static final int RESULT = 1;
     public ReturnNode(Block block, Node sideEffect, Node result) {
-        // TODO: SideEffect
         super(block, result);
     }
 
